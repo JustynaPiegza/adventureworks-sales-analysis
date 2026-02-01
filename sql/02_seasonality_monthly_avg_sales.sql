e71b6e17d9f6014d42e21a4c32e8d2559f6144e8
JustynaPiegza <jjozefowska@gmail.com>
2026-02-01 17:17:38 +0100

    Add monthly seasonality analysis with order count and average order value

--- sql/02_seasonality_monthly_avg_sales.sql
+++ sql/02_seasonality_monthly_avg_sales.sql
@@ -2,13 +2,15 @@
 -- Is there a seasonal pattern in monthly sales?
 
 -- Description:
--- Average sales value is calculated per calendar month
--- Data from all years is aggregated together
+-- Average order value and number of orders are calculated per calendar month 
+-- Data from all years is aggregated together 
 -- OrderDate month is used to identify seasonality
+-- Results allow comparison of customer activity and order value by month
 
 SELECT 
 	MONTH(OrderDate) AS order_month,
-	ROUND(AVG(TotalDue),2) AS avg_monthly_sales
+	COUNT (*) AS number_of_orders,
+	ROUND(AVG(TotalDue),2) AS avg_order_value
 FROM Sales.SalesOrderHeader
 GROUP BY 
 	MONTH(OrderDate)
